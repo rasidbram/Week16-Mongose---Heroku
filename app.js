7501d7e525380a5a41e0943974822f3c6ef61a56
Your Name <rashidaynur.ibram@yandex.com>
2019-10-17 13:02:13 +0100

    changed list

--- app.js
+++ app.js
@@ -21,15 +21,15 @@ const Item = mongoose.model("Item", itemsSchema);
 
 
 const item1 = new Item({
-  name: "Welcome to your todolist!"
+  name: "get up early"
 });
 
 const item2 = new Item({
-  name: "Hit the + button to add a new item."
+  name: "get ready"
 });
 
 const item3 = new Item({
-  name: "<-- Hit this to delete an item."
+  name: "study hard"
 });
 
 const defaultItems = [item1, item2, item3];
